a7896b7f8c19b3358526a6f6951fc8995335c3da
Dennis Wilson <d9w@mit.edu>
2015-02-04 22:18:42 +0200

    basic port of model definitions

--- flask_app/models.py
+++ flask_app/models.py
@@ -3,4 +3,34 @@ from flask.ext.sqlalchemy import SQLAlchemy
 
 db = SQLAlchemy(app)
 
-### Add models here
+class Host(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(255))
+    mac = db.Column(db.String(100), unique=True)
+    group_id = db.Column(db.Integer, db.ForeignKey('group.id'))
+
+class Group(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(255))
+    hosts = db.relationship('Host', backref='group', lazy='dynamic')
+    server_id = db.Column(db.Integer, db.ForeignKey('dhcpserver.id'))
+
+class Subnet(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(255))
+    netmask = db.Column(db.String(255))
+    broadcast_address = db.Column(db.String(255))
+    routers = db.Column(db.String(255))
+    ips = db.relationship('IP', backref='subnet', lazy='dynamic')
+    server_id = db.Column(db.Integer, db.ForeignKey('dhcpserver.id'))
+
+class IP(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    address = db.Column(db.String(255))
+    subnet_id = db.Column(db.Integer, db.ForeignKey('subnet.id'))
+
+class DHCPServer(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    hostname = db.Column(db.String(255))
+    groups = db.relationship('Group', backref='server', lazy='dynamic')
+    subnets = db.relationship('Subnet', backref='server', lazy='dynamic')
