1de62ea0e18a3060486f95b11266d87dcb3e358c
Dennis Wilson <d9w@mit.edu>
2015-03-02 19:39:35 +0200

    json errors for api

--- flask_app/errors.py
+++ flask_app/errors.py
@@ -1,12 +1,20 @@
 from .app import app
+from .models import db
 
-from flask import render_template, make_response
+from flask import jsonify, render_template, make_response, request
 
-def _define_custom_error_page(code):
+def _define_custom_error_page(code, message=None):
     @app.errorhandler(code)
     def _handler(err):
+        print request.path
+        if code == 500:
+            db.session.rollback()
+        if request.path.startswith('/api'):
+            response = jsonify(dict({"status":code,"message":message}))
+            response.status_code = code
+            return response
         return make_response(render_template("errors/{}.html".format(code)), code)
 
-_define_custom_error_page(500)
-_define_custom_error_page(404)
-_define_custom_error_page(403)
+_define_custom_error_page(500,"internal server error")
+_define_custom_error_page(404,"page not found")
+_define_custom_error_page(403,"forbidden")
