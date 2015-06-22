935793186aed2211e5755bd109beb83ecceb722e
Mark Vasilkov <mvasilkov@gmail.com>
2015-06-22 19:56:02 +0300

    Client app: Groups.

---
+++ client/app/javascripts/views/groups.js
@@ -0,0 +1,41 @@
+define([
+    'backbone',
+    'backgrid',
+    'collections',
+    'text!templates/_groups.html'
+], function (Backbone, Backgrid, collections, template) {
+    var columns = [
+        {
+            name: 'id',
+            cell: 'integer',
+            editable: false
+        },
+        {
+            name: 'name',
+            cell: 'string',
+            editable: false
+        },
+        {
+            name: 'dn',
+            cell: 'string',
+            editable: false
+        },
+    ]
+
+    return Backbone.View.extend({
+        initialize: function () {
+            this.collection = new collections.GroupCollection
+
+            this.grid = new Backgrid.Grid({
+                columns: columns,
+                collection: this.collection
+            })
+        },
+
+        render: function () {
+            this.$el.html(template)
+            this.$('.view-table').append(this.grid.render().$el.addClass('table'))
+            this.collection.fetch({reset: true})
+        }
+    })
+})
--- client/app/javascripts/views/index.js
+++ client/app/javascripts/views/index.js
@@ -1,4 +1,7 @@
-define(['backbone', 'text!templates/_index.html'], function (Backbone, template) {
+define([
+    'backbone',
+    'text!templates/_index.html'
+], function (Backbone, template) {
     return Backbone.View.extend({
         render: function () {
             this.$el.html(template)
